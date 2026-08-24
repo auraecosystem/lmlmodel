06b85bb595b9ea9b49fe00889b2fe7814de375a0
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-24 17:09:01 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,3 +3,8 @@ bazel build //lmlmodel:lmlmodel
 
 # Run all unit tests
 bazel test //tests/...
+# Generate or update requirements_lock.txt from requirements.in
+bazel run //:requirements.update
+
+# Verify that requirements_lock.txt is up-to-date with requirements.in (ideal for CI pipelines)
+bazel test //:requirements_test
